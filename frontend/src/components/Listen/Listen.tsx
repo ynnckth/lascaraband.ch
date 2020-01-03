d0348ba3fbccb522a3d078c51f71594c0e01f2de
ynnck <o5YV;)YsA4,L6#D>
2020-01-03 23:38:56 +0100

    Embed facebook video

--- frontend/src/components/Listen/Listen.tsx
+++ frontend/src/components/Listen/Listen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Listen.css';
 
 // TODO:
 //  - embed bandcamp widget with single (as soon as released)
@@ -8,8 +8,14 @@ import React from 'react';
 const Listen: React.FC = () => {
   return (
     <div className="listen">
-      <h1>Upcoming gigs</h1>
-      <p>coming soon...</p>
+      <h1>Listen</h1>
+
+      <iframe
+        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fsuperscaras%2Fvideos%2F657849404639648%2F&show_text=0&width=560"
+        className="facebook-embedded-video"
+        width="560" height="315" scrolling="no" frameBorder="0"
+        >
+      </iframe>
     </div>
   );
 };
