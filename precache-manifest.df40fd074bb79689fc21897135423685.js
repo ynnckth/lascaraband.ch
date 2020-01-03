b525bc3059a6c5bc12cae18f7c128aa35a65ac2f
ynnck <o5YV;)YsA4,L6#D>
2020-01-03 21:33:12 +0100

    Updates

---
+++ precache-manifest.df40fd074bb79689fc21897135423685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6e767055fdc76a87a262d9ca46c0a4",
+    "url": "/scara/index.html"
+  },
+  {
+    "revision": "a15c9aee896404e73b0c",
+    "url": "/scara/static/css/main.90a1a5fd.chunk.css"
+  },
+  {
+    "revision": "67e2915abf1e51b6a2b1",
+    "url": "/scara/static/js/2.90b081a8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/scara/static/js/2.90b081a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a15c9aee896404e73b0c",
+    "url": "/scara/static/js/main.7256c0fb.chunk.js"
+  },
+  {
+    "revision": "e4a6d87e476cd0a3dc6e",
+    "url": "/scara/static/js/runtime-main.260a6f02.js"
+  }
+]);
