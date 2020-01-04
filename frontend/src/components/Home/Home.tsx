68df3757a028cb6d0248e2e8bbb4a041171e4af0
ynnck <o5YV;)YsA4,L6#D>
2020-01-04 18:34:46 +0100

    Add image to home page

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Home.css';
 import Gigs from '../Gigs/Gigs';
+import titleImage from '../../assets/images/18402279_1437491599651645_3253204915988598215_o.jpg';
 
 
 const Home: React.FC = () => {
   return (
     <div className="home">
+      <img src={titleImage} className="home-image" alt="home" />
+
       <Gigs showPastGigs={false}/>
 
       <div className="instagram-feed">
