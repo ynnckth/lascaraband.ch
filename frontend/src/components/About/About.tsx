6d137ecff51a3130ca08e5f43206b0e0adb5510a
ynnck <o5YV;)YsA4,L6#D>
2020-01-05 15:34:04 +0100

    Adapt about text and add image

--- frontend/src/components/About/About.tsx
+++ frontend/src/components/About/About.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import './About.css';
+import aboutImage from '../../assets/images/0130_15578793_1285138484886958_1103544731450889802_n.jpg';
 
-
-// TODO:
-//  - description text
-//  - pics and names of band members
 
 const About: React.FC = () => {
   return (
     <div className="about">
       <h1>About</h1>
-      <p>La Scaraband was born in 2016 ...</p>
+      <p>La Scaraband was born in 2016 and consists of Leonardo Pedrocca on guitar, Jorge Oswald on guitar and bass,
+        Carolina Segui on clarinet, Ivan Garcia on percussion and Yannick Streit on drums.</p>
+      <br/>
+      <img src={aboutImage} className="about-image" alt="the band"/>
     </div>
   );
 };
